ea888c5e4e49d09ecba6eb4a3f5c2c42f66c5f6f
Noa Charbogne <noa.charbogne@orange.fr>
2024-11-05 13:43:23 +0100

    all

---
+++ push_swap.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncharbog <ncharbog@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/05 10:03:39 by ncharbog          #+#    #+#             */
+/*   Updated: 2024/11/05 10:06:21 by ncharbog         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+
+typedef struct s_list
+{
+	int	nb;
+	struct	s_list	*prev;
+	struct	s_list	*next;
+}					t_list;
+
+#endif
